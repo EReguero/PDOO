fb9a1f96fef4291c6e87474908d0eb72c1a74ec8
Dunia Rodríguez <dunia.du.d@gmail.com>
2016-11-10 14:20:07 +0100

    Create P2_Napakalaki.rb

---
+++ P2_Napakalaki.rb
@@ -0,0 +1,69 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+#require?
+include Singleton
+
+class Napakalaki
+  napa = MiClase.instance
+  
+  def initialize(cp, undealer, uncurrent_monster)
+    @current_player = cp
+    @players = Array.new
+    @dealer = undealer
+    @current_monster = uncurrent_monster
+    
+  end
+  
+  private 
+  def next_player
+    
+  end
+  
+  private 
+  def next_turn_allowed
+    
+  end
+  
+  private
+  def set_enemies
+    
+  end
+  
+  def develop_combat
+    
+  end
+  
+  def discard_visible_treasures(treasures)
+    
+  end
+  
+  def discard_hidden_treasures(treasures)
+    
+  end
+  
+  def make_treasures_visible(treasures)
+    
+  end
+  
+  def init_game(players)
+    
+  end
+  
+  def get_current_player
+    
+  end
+  
+  def get_current_monster
+    
+  end
+  
+  def next_turn
+    
+  end
+  
+  def end_of_game(result)
+    
+  end
+  
+end
