6f613b220a1fa5d092a57d6d0e1fe939043e6acd
Dunia Rodríguez <dunia.du.d@gmail.com>
2016-11-10 14:17:48 +0100

    Create P2_CardDealer.rb

---
+++ P2_CardDealer.rb
@@ -0,0 +1,18 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+include Singleton
+
+class CardDealer
+  include Singleton
+  def initialize
+    
+  end
+  
+  def suffleTreasures()
+    @unusedTreasures
+  end
+  
+  #givemonsterback @unusedMonster
+  #--m
+end
