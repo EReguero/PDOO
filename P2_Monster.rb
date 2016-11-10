79bfe75d4fcf6f6300080957fc2c2ea55860551f
Dunia Rodríguez <dunia.du.d@gmail.com>
2016-11-10 14:19:32 +0100

    Create P2_Monster.rb

---
+++ P2_Monster.rb
@@ -0,0 +1,22 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+encoding:: utf-8
+
+require_relative "prize.rb"
+require_relative "bad_consequence.rb"
+
+
+
+
+class Monster
+  attr_reader :name, :level, :bd, :prize
+  
+  def initialize (name, level, bc, prize)
+    @name=name
+    @level=level
+    @badConsequence = bc
+    @prize = prize
+  end
+  
+end
